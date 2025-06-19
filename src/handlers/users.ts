f97e645a21a8c1589d17141456ef67ba012dd795
regiss05 <regiss.steven@gmail.com>
2025-06-19 16:19:09 +0200

    users.ts

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,52 +1,51 @@
-import { Router } from "express";
-
+import { Router, Request, Response } from "express";
 import platformAPIClient from "../services/platformAPIClient";
 
-
 export default function mountUserEndpoints(router: Router) {
-  // handle the user auth accordingly
-  router.post('/signin', async (req, res) => {
+  router.post("/signin", async (req: Request, res: Response): Promise<void> => {
     const auth = req.body.authResult;
     const userCollection = req.app.locals.userCollection;
 
     try {
       // Verify the user's access token with the /me endpoint:
-      const me = await platformAPIClient.get(`/v2/me`, { headers: { 'Authorization': `Bearer ${auth.accessToken}` } });
-      console.log(me);
+      const me = await platformAPIClient.get(`/v2/me`, {
+        headers: { Authorization: `Bearer ${auth.accessToken}` },
+      });
+      console.log(me.data);
     } catch (err) {
-      console.log(err);
-      return res.status(401).json({error: "Invalid access token"}) 
+      console.error(err);
+      res.status(401).json({ error: "Invalid access token" });
+      return;
     }
 
     let currentUser = await userCollection.findOne({ uid: auth.user.uid });
 
     if (currentUser) {
-      await userCollection.updateOne({
-        _id: currentUser._id
-      }, {
-        $set: {
-          accessToken: auth.accessToken,
+      await userCollection.updateOne(
+        { _id: currentUser._id },
+        {
+          $set: {
+            accessToken: auth.accessToken,
+          },
         }
-      });
+      );
     } else {
       const insertResult = await userCollection.insertOne({
         username: auth.user.username,
         uid: auth.user.uid,
         roles: auth.user.roles,
-        accessToken: auth.accessToken
+        accessToken: auth.accessToken,
       });
-      
+
       currentUser = await userCollection.findOne(insertResult.insertedId);
     }
 
     req.session.currentUser = currentUser;
-
-    return res.status(200).json({ message: "User signed in" });
+    res.status(200).json({ message: "User signed in" });
   });
 
-  // handle the user auth accordingly
-  router.get('/signout', async (req, res) => {
+  router.get("/signout", async (req: Request, res: Response): Promise<void> => {
     req.session.currentUser = null;
-    return res.status(200).json({ message: "User signed out" });
+    res.status(200).json({ message: "User signed out" });
   });
 }
